7f8be8d39d4b04229f7803aa7f77f0c8d6cb4081
kapupa <kurkeu@gmail.com>
2023-10-30 08:17:27 +0100

    Add project files.

---
+++ Sorter/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Sorter
+{
+    public partial class MainWindow : Window
+    {     
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image files|*.bmp;*.jpg;*.png";
+            openDialog.FilterIndex = 1;
+
+            if (openDialog.ShowDialog() == true)
+            {
+                imagePicture.Source = new BitmapImage(new Uri(openDialog.FileName));
+            }
+            int i = 0;
+        }
+
+    }
+}
